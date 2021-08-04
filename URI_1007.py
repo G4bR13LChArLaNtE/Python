e25787c6bd7a8ce8620237b8d5a4b0d9ddb0dee2
Gabriel Charlante <gabrielcharlante@gmail.com>
2021-08-03 21:35:01 -0300

    gitignore file

---
+++ URI_1007.py
@@ -0,0 +1,14 @@
+#URI-1007 - Diferença:
+
+#Entradas:
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+#Processamento dos dados:
+
+DIFERENCA = (A*B - C*D)
+
+print("DIFERENCA = {}".format(DIFERENCA))
